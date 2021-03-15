264322e6b034fdc8873bb7d0abd2c31159029777
tri-bui <buitri91@gmail.com>
2021-03-14 17:00:47 -0700

    Add line plots to ggplot_demo.R

--- r-analysis/ggplot_demo.R
+++ r-analysis/ggplot_demo.R
@@ -10,14 +10,38 @@ mpg_data <- mpg
 
 ### Bar plots ###
 
-# Bar plot
-bar_plt <- ggplot(mpg, aes(x = class))
-bar_plt + geom_bar()
+# Bar plot for category counts
+bar1 <- ggplot(mpg, aes(x = manufacturer))
+bar1 + geom_bar() + ggtitle('Vehicles by Manufacturer') + 
+       xlab('Manufacturer') + ylab('Number of Vehicles') +
+       theme(axis.text.x = element_text(angle = 45, 
+                                        hjust = 1))
 
-# Count cars from each class
-manu_count <- mpg_data %>% group_by(class) %>% 
-                           summarize(n_cars = n())
+# Calculate the mean mpg for each class
+manu_mpg <- mpg_data %>% group_by(manufacturer) %>% 
+                          summarize(mean_hwy = mean(hwy))
 
-# Same bar plot using 2 variables
-bar_plt <- ggplot(manu_count, aes(x = class, y = n_cars))
-bar_plt + geom_col()
+# Bar plot for a different metric
+bar2 <- ggplot(manu_mpg, aes(x = manufacturer, y = mean_hwy))
+bar2 + geom_col() + ggtitle('Average MPG by Manufacturer') + 
+       xlab('Manufacturer') + 
+       ylab('Average Fuel Economy (MPG)') + 
+       theme(axis.text.x = element_text(angle = 45, 
+                                        hjust = 1))
+
+
+### Line plots ###
+
+# Calculate the mean mpg by cylinder count for Toyota cars
+toyota_mpg <- mpg_data %>% 
+                subset(manufacturer == 'toyota') %>% 
+                group_by(cyl) %>% 
+                summarize(mean_hwy = mean(hwy))
+
+# Line plot
+line1 <- ggplot(toyota_mpg, aes(x = cyl, y = mean_hwy))
+line1 + geom_line() + ggtitle('Average MPG of Toyota Cars') + 
+        xlab('Number of Cylinders') + 
+        ylab('Average Fuel Economy (MPG)') + 
+        scale_x_discrete(limits = c(4, 6, 8)) + 
+        scale_y_continuous(breaks = 15:30)
