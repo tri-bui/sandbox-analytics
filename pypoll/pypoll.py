98491422685dcb519410ded09debd014fa8bb7f1
tri-bui <buitri91@gmail.com>
2021-04-10 15:46:42 -0700

    Refactor pypoll/pypoll.py
    - Change horizontal rule function to variables
    - Store results to variables before writing to file
    - Print results to file
    - Results file formatting

--- pypoll/pypoll.py
+++ pypoll/pypoll.py
@@ -51,7 +51,7 @@ largest, county_most_votes = '', 0
 
 # Vote % from each county
 for county, votes in county_cnts.items():
-    county_pcts[county] = round(votes / total_votes * 100, 2)
+    county_pcts[county] = votes / total_votes * 100
 
     # Largest county turnout
     if votes > county_most_votes:
@@ -64,7 +64,7 @@ winner, candi_most_votes = '', 0
 
 # Vote % each candidate received
 for candi, votes in candi_cnts.items():
-    candi_pcts[candi] = round(votes / total_votes * 100, 2)
+    candi_pcts[candi] = votes / total_votes * 100
 
     # Winner
     if votes > candi_most_votes:
@@ -73,40 +73,47 @@ for candi, votes in candi_cnts.items():
 
 """ Write Results to File """
 
-def write_hr(file, n=1):
+# Horizontal rules with newline pipes
+np = '\n|' # newline pipe
+hr = np + ('-' * 50) + np # 1 line
+hr2 = hr + hr[2:] # 2 lines
 
-    """ Write a horizontal rule to the output file n number of times. """
+# Title and total votes
+heading = (
+    f'{hr2[1:]}\tElection Results{hr}'
+    f'\tTotal votes: {total_votes:,}{hr2}'
+)
 
-    for i in range(n):
-        file.write('----------------------------------------------\n')
+# Vote count for each county
+county_votes = f'{np}{hr2}\tCounties{hr}'
+for county, votes in county_cnts.items():
+    county_votes += f'\t{county} County: {votes:,} votes ({county_pcts[county]:.2f}%){np}'
 
+# Largest county turnout
+largest_turnout = (
+    f'{hr[2:]}\tLargest Turnout: {largest} County{np}'
+    f'\tVotes: {county_most_votes:,} ({county_pcts[largest]:.2f}%){hr2}'
+)
+
+# Vote count for each candidate
+candidate_votes = f'{np}{hr2}\tCandidates{hr}'
+for candi, votes in candi_cnts.items():
+    candidate_votes += f'\t{candi}: {votes:,} votes ({candi_pcts[candi]:.2f}%){np}'
+
+# Election winner
+election_winner = (
+    f'{hr[2:]}\tWinner: {winner}{np}'
+    f'\tVotes: {candi_most_votes:,} ({candi_pcts[winner]:.2f}%){hr2[:-1]}'
+)
+
+
+# Print results
+print(heading + county_votes + largest_turnout + candidate_votes + election_winner)
 
 # Write to file
 with open(analysis_path, 'w') as write_file:
-    write_file.write('\nElection Results\n')
-    write_hr(write_file)
-    write_file.write(f'Total votes: {total_votes:,}\n')
-    write_hr(write_file, 2)
-
-    # Vote count for each county
-    write_file.write('\nCounties\n')
-    write_hr(write_file)
-    for county, votes in county_cnts.items():
-        write_file.write(f'{county} County: {votes:,} votes ({county_pcts[county]}%)\n')
-    write_hr(write_file)
-
-    # Largest county turnout
-    write_file.write(f'Largest County Turnout: {largest}\n')
-    write_file.write(f'Votes: {county_most_votes:,} ({county_pcts[largest]}%)\n')
-    write_hr(write_file, 2)
-
-    # Election winner
-    write_file.write('\nCandidates\n')
-    write_hr(write_file)
-    for candi, votes in candi_cnts.items():
-        write_file.write(f'{candi}: {votes:,} votes ({candi_pcts[candi]}%)\n')
-    write_hr(write_file, 1)
-
-    write_file.write(f'Winner: {winner}\n')
-    write_file.write(f'Votes: {candi_most_votes:,} ({candi_pcts[winner]}%)\n')
-    write_hr(write_file, 2)
+    write_file.write(heading) # title and total votes
+    write_file.write(county_votes) # vote counts for each county
+    write_file.write(largest_turnout) # largest county turnout
+    write_file.write(candidate_votes) # vote counts for each candidate
+    write_file.write(election_winner) # election winner
